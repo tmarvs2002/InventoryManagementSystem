e527c13888187794e096a68c43c56840332f570a
Tom Marvin <tom.marvin2@gmail.com>
2024-01-02 05:23:19 +0000

    refresh

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    swcMinify: true,
+    distDir: 'build',
+    compiler: {
+        styledComponents: true
+    },
+    webpack: (config, { webpack, isServer, nextRuntime }) => {
+        if (isServer && nextRuntime === "nodejs")
+            config.plugins.push(
+                new webpack.IgnorePlugin({ resourceRegExp: /^aws-crt$/ })
+            );
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: [{ loader: '@svgr/webpack', options: { icon: true, typescript: true, ref: true } }],
+        })
+
+        return config
+    },
+}
 
 module.exports = nextConfig
